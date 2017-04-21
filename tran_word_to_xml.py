1c66e39660786450b3034f9cfa9f6000de221b59
lfkdsk <lfk_dsk@hotmail.com>
2017-04-21 15:14:59 +0800

    update by script

---
+++ tran_word_to_xml.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from xml.dom.minidom import Document
+
+def read_file(file_name):
+    for line in open(file_name):
+        if '##' not in line and '----' not in line and line.startswith('|'):
+            line = line.replace(' ', '')
+            if not line.startswith('||||'):
+                parser_line(line[1:])
+    pass
+
+def check_invalid(file_name):
+    file = open(file_name, 'r')
+    first_line = file.readline()
+    file.close();
+    if not first_line.startswith('# - used - '):
+        read_file(file_name);
+        pass
+
+def parser_line(line):
+    index = line.find('|')
+    global last_item
+    global word_dict
+    if index > 0:
+        word = line[:index]
+        if not line.startswith('Word') and not line.startswith('word'):               
+            word_item = [word, line[index:].replace('|',' ')]
+            last_item = word_item
+            word_dict.append(word_item)
+    elif index == 0:
+        last_item[1] += '\n' + line[index:].replace('|',' ')
+
+# initial and read file
+def initial_search():
+    print sys.argv[1]
+    check_invalid(sys.argv[1])
+
+    contentText = ""
+    doc = Document()  #创建DOM文档对象
+    word_book = doc.createElement('wordbook') #创建根元素
+    doc.appendChild(word_book)
+    for word_item in word_dict:
+        item = doc.createElement('item')
+        word = doc.createElement('word')
+        trans = doc.createElement('trans')
+
+        word.appendChild(doc.createTextNode(word_item[0]))
+        trans.appendChild(doc.createTextNode(word_item[1].encode('utf-8')))
+        item.appendChild(word)
+        item.appendChild(trans)
+        word_book.appendChild(item)
+        pass
+
+    out_file = open('output/' + sys.argv[1].replace('.md', '_xml.xml'),'w')
+    out_file.write(doc.toprettyxml(indent = ''))
+    out_file.close()
+    pass
+
+global word_dict
+
+global last_item
+
+word_dict = []
+last_item = []
+
+initial_search()
+
